983477e2d277e562081da38fe80e5de46dc348f3
kenzyyasserr <kanzy.yasser.mohammed@gmail.com>
2025-08-29 04:40:27 +0300

    main function

--- task 8,2.cpp
+++ task 8,2.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <thread>  //for the delay
+#include <chrono>
+
 
 using namespace std;
 
@@ -64,3 +67,44 @@ public:
         cout << opponent.name << " health: " << opponent.health << "\n\n";
     }
 };
+
+//main function----------------------------------------------------
+int main() {
+    srand(time(0));
+
+    Character optimus("Optimus Prime", 100);
+    Character megatron("Megatron", 100);
+
+    //Optimus' weapons
+    optimus.weapons[0] = weapon("Ion Rifle", 6, 100);
+    optimus.weapons[1] = weapon("Energon Swords", 12, 80);
+    optimus.weapons[2] = weapon("Shoulder Cannon", 45, 25);
+
+    //Megatron's weapons
+    megatron.weapons[0] = weapon("Fusion Cannon", 9, 90);
+    megatron.weapons[1] = weapon("Fusion Sword", 18, 70);
+    megatron.weapons[2] = weapon("Tank Mode", 60, 15);
+
+    cout << "Optimus Prime vs Megatron\n";
+    cout << "Let the fight begin!\n\n";
+
+    bool optimusTurn = true;
+    while (optimus.isAlive() && megatron.isAlive()) {
+        int choice = rand() % 3; //picking a random weapon
+        if (optimusTurn) {
+            optimus.attack(megatron, choice);
+        } else {
+            megatron.attack(optimus, choice);
+        }
+        optimusTurn = !optimusTurn;
+
+        this_thread::sleep_for(chrono::seconds(2));
+    }
+
+    if (optimus.isAlive())
+        cout << "Fight's over and Optimus Prime is the WINNER!\n";
+    else
+        cout << "Fight's over and Megatron is the WINNER!\n";
+
+    return 0;
+}
